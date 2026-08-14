4c6452ddce45254d6cad0c41770e17983927210d
kokila-jana <kokilagur2016@gmail.com>
2026-08-14 11:12:38 +0530

    Create Exceptionhandling.java with exception examples
    Added a Java class for exception handling examples, including comments on various exception types and handling mechanisms.

---
+++ Exceptionhandling.java
@@ -0,0 +1,90 @@
+package morning;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exceptionhandling {
+
+	public static void main(String[] args) //throws InterruptedException
+	{
+	
+//		for(int i=1;i<=10;i++)
+//		{
+//			System.out.println(i);
+//			Thread.sleep(1000);
+//		}
+		
+	/*
+	 * Error
+	 * 3 types
+	 * 1.compile time-
+	 * 2.runtime-->to allow run the program/
+	 * to block the flow of the program
+	 * 
+	 * 3.logical
+	 */
+//		System.out.println("Hello");
+//		Scanner sc=new Scanner(System.in);
+//		int a=sc.nextInt();//InputMismatchException
+//		int b=sc.nextInt();//ArithmeticException
+//		int c=a/b;
+//		System.out.println(c);
+//		System.out.println("java");
+		
+//		int a[]=new int[3];
+//		a[0]=3;
+//		a[1]=2;
+//		a[2]=1;
+//		a[3]=0;//ArrayIndexOutOfBoundsException
+//		System.out.println("Hello");
+			
+//		String s="Java";
+//		String s=null;//NullPointerException
+//		System.out.println(s.charAt(4));
+		//StringIndexOutOfBoundsException
+		
+		
+		
+		
+//		Scanner sc=new Scanner(System.in);
+//		try
+//		{
+//		int a=sc.nextInt();//InputMismatchException
+//		int b=sc.nextInt();//ArithmeticException
+//		int c=a/b;
+//		System.out.println(c);
+//		}
+//		catch(ArithmeticException e)
+//		{
+//			System.out.println(e);
+//		}
+//		catch(InputMismatchException e1)
+//		{
+//			System.out.println(e1);
+//		}
+//		finally
+//		{
+//			System.out.println("Hello");
+//			System.out.println("java");
+//		}
+		
+//		Scanner sc=new Scanner(System.in);
+//		int age=sc.nextInt();
+//		if(age>=18)
+//		{
+//			System.out.println("Eligible for voting...");
+//		}
+//		else
+//		{
+//			throw new InputMismatchException("Not Eligible for voting...");
+////			System.out.println("Not Eligible for voting...");
+//		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
